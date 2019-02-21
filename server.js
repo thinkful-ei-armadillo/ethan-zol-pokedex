a06932d0a3234f0dedad79ff43056dc112d206f3
Ethan Zimmerman <thebinarypenguin@gmail.com>
2019-02-21 12:10:57 -0500

    Add auth middleware

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require('express');
 const morgan = require('morgan');
 
@@ -5,9 +7,35 @@ const app = express();
 
 app.use(morgan('dev'));
 
-app.use((req, res) => {
-  res.send('Hello, world!');
+app.use(function validateBearerToken(req, res, next) {
+
+  const authToken = req.get('Authorization');
+  const apiToken = process.env.API_TOKEN;
+
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    return res.status(401).json({ error: 'Unauthorized request' });
+  }
+
+  next();
 });
+
+const validTypes = ['Bug', 'Dark', 'Dragon', 'Electric', 'Fairy', 'Fighting', 'Fire', 'Flying', 'Ghost', 'Grass', 'Ground', 'Ice', 'Normal', 'Poison', 'Psychich', 'Rock', 'Steel', 'Water'];
+
+function handleGetTypes(req, res) {
+  res.json(validTypes);
+}
+
+app.get('/types', handleGetTypes);
+
+
+
+function handleGetPokemon(req, res) {
+  res.send('Hello, Pokemon!');
+}
+
+app.get('/pokemon', handleGetPokemon);
+
+
 
 const PORT = 8000;
 
