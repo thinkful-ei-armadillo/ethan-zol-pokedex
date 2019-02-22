dc45f2fb9fdd220766f1af6dcd2355b98bad3a00
Ethan Zimmerman <thebinarypenguin@gmail.com>
2019-02-22 16:48:35 -0500

    Update with Heroku config

--- server.js
+++ server.js
@@ -8,10 +8,10 @@ const POKEDEX = require('./pokedex.json');
 
 const app = express();
 
-app.use(morgan('dev'));
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting));
 app.use(helmet());
 app.use(cors());
-
 
 app.use(function validateBearerToken(req, res, next) {
 
@@ -68,9 +68,17 @@ function handleGetPokemon(req, res) {
 
 app.get('/pokemon', handleGetPokemon);
 
+app.use((error, req, res, next) => {
+  let response;
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' }};
+  } else {
+    response = { error };
+  }
+  res.status(500).json(response);
+});
 
-
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
